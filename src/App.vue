cf2979f2c95c03c73f317f0f12b8e7023d291f2f
Thom Vincent <hello@doabackflip.com>
2021-09-12 13:02:21 +1000

    Add design assets and fonts.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1 class="font-medium">Audience Republic</h1>
     <router-link to="/">Home</router-link>
     <router-link to="/editor">Editor</router-link>
     <router-link to="/events">Events</router-link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        regular: ['"Graphik Regular"'],
+        medium: ['"Graphik Medium"'],
+      },
+    },
   },
   variants: {
     extend: {},
