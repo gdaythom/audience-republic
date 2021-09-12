9f4b467f6a899ae226916866c7be975759230a3f
Thom Vincent <hello@doabackflip.com>
2021-09-12 19:36:17 +1000

    Add basic styling for the button.

---
+++ src/components/ARButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button>
+    <slot name="icon" />
+    <slot name="label" />
+  </button>
+</template>
+
+<script>
+export default {
+	name: 'ARButton',
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  background-color: #7344C0;
+  border-radius: 3px;
+  box-sizing: border-box;
+  color: #ffffff;
+  font-size: 14px;
+  line-height: 50px;
+  height: 50px;
+  text-align: center;
+  transition: background .2s ease;
+  width: 190px;
+  &:hover {
+    background-color: #592F9D;
+    transition: background .2s ease;
+  }
+}
+</style>
