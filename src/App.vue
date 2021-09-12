14b3b7cf143b0e48908a3be8d8037600b4ab2630
Thom Vincent <hello@doabackflip.com>
2021-09-12 12:24:40 +1000

    Add pages to project.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="app">
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/editor">Editor</router-link>
+    <router-link to="/events">Events</router-link>
     {{ isLoading }}
     <pre>{{ events }}</pre>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -27,25 +26,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
