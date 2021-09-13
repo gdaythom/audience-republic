3aaafa53634651d671a3cef6f23b33f32a913a41
Thom Vincent <hello@doabackflip.com>
2021-09-13 13:06:42 +1000

    Add header component.

---
+++ src/components/ARHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <div class="title">
+      <slot name="title" />
+    </div>
+    <div class="afeature">
+      <slot name="afeature" />
+    </div>
+    <div class="avatar">
+      <slot name="avatar" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+	name: 'ARHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  position: relative;
+}
+.title {
+  background-color: #7344C0;
+  text-align: center;
+  padding: 15px 0;
+}
+.afeature {
+  height: 195px;
+  overflow: hidden;
+  img {
+    width: 100%;
+  }
+}
+.avatar {
+  border-radius: 100%;
+  height: 60px;
+  overflow: hidden;
+  position: absolute;
+  left: 50%;
+  bottom: -10px;
+  transform: translateX(-50%);
+  width: 60px;
+  img {
+    width: 100%;
+  }
+}
+</style>
--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,11 +1,61 @@
 <template>
-  <div>
-    <h1>This is an editor page</h1>
+  <div class="wrapper">
+    <ARHeader>
+      <template v-slot:title>
+        <h1>Brightness & Contrast Developer Test</h1>
+        <h2>01 Jun, 2018 – 31 Dec, 2019</h2>
+      </template>
+      <template v-slot:afeature>
+        <img
+          src="/images/beyond-the-valley@2x.jpg"
+          alt="Beyond the Valley"
+        >
+      </template>
+      <template v-slot:avatar>
+        <img
+          src="/images/avatar.jpg"
+          alt="Thom Vincent"
+        >
+      </template>
+    </ARHeader>
   </div>
 </template>
 
 <script>
+import ARHeader from '../components/ARHeader.vue';
+
 export default {
 	name: 'Editor',
+	components: {
+		ARHeader,
+	},
 };
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/_variables.scss';
+.wrapper {
+  background-color: red;
+  margin: 0 auto;
+  max-width: 375px;
+  width: 100%;
+}
+h1 {
+  color: #ffffff;
+  font-family: 'GraphikMedium';
+  font-size: 15px;
+  font-weight: normal;
+  line-height: 25px;
+  margin: 0;
+  padding: 0;
+}
+h2 {
+  color: #ffffff;
+  font-family: 'GraphikRegular';
+  font-size: 13px;
+  font-weight: normal;
+  line-height: 20px;
+  margin: 0;
+  padding: 0;
+}
+</style>
